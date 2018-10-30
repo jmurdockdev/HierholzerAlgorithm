46540f59b472f8e79ed4b5897fc7e7716b455d83
jmurdockdev <jmurdockdev@gmail,com>
2018-10-30 10:09:29 -0400

    Refactor main.cpp

--- UC_Discrete_HW_3_1/main.cpp
+++ UC_Discrete_HW_3_1/main.cpp
@@ -7,14 +7,13 @@
 
 using namespace std;
 
-void PrintEulerianPath(vector< vector<int> > graph)
+void PrintEulerianPath(vector<vector<int>> graph)
 {
 	//count of edges
 	unordered_map<int, int> count;
 
 	for (int i = 0; i<graph.size(); i++)
 	{
-	
 		count[i] = graph[i].size();
 	}
 
@@ -22,7 +21,7 @@ void PrintEulerianPath(vector< vector<int> > graph)
 		return; 
  
 	stack<int> currentPath;
-	vector<int> circuit;
+	vector<int> eulerCircuit;
 
 	currentPath.push(0);
 	int currentVertex = 0;  
@@ -46,47 +45,49 @@ void PrintEulerianPath(vector< vector<int> > graph)
 		
 		else
 		{
-			circuit.push_back(currentVertex);
+			eulerCircuit.push_back(currentVertex);
 
-			
 			currentVertex = currentPath.top();
 			currentPath.pop();
 		}
 	}
 
 	
-	for (int i = circuit.size() - 1; i >= 0; i--)
+	for (int i = eulerCircuit.size() - 1; i >= 0; i--)
 	{
-		cout << circuit[i];
+		cout << eulerCircuit[i];
 		if (i)
-			cout << " ";
+			cout << " - ";
 	}
 }
 
 
-
-
 int main()
 {
+
 	while (1) {
 
 	cout << "********************************************************" << endl;
+	cout << endl;
 	cout << "Hierholzer's algorithm for computing an Eulerian circuit" << endl;
+	cout << endl;
 	cout << "********************************************************" << endl;
 
 	std::string source;
 	vector<int> numbers;
 
-	cout << "Enter numbers separated by spaces \n"
+	cout << endl;
+	cout << "Enter numbers separated by spaces. \n"
 		<< "The first number is the size of the graph, \n"
-		<< "followed by pairs representing the edge set: ";
-	std::getline(std::cin, source);
+		<< "followed by pairs representing the edge set: \n>> ";
+
+	getline(cin, source);
+	cin.clear();
 
 	for (int i = 0; i < source.length(); i++) {
 		char c = source[i];
 		if (isdigit(c)) {
 			int num = (int) c - 48;
-			
 			numbers.push_back(num);
 		}
 	}
@@ -96,22 +97,20 @@ int main()
 	int size = numbers[0];
 	graph.resize(size);
 	
-
 	numbers.erase(numbers.begin());
 
 	for (int i = 0; i < numbers.size(); i += 2)
 	{
 		int v1 = numbers[i];
 		int v2 = numbers[i + 1];
-
 		graph[v1].push_back(v2);
 	}
 
-
+	cout << "Euclerian Circuit: ";
 	PrintEulerianPath(graph);
 	cout << endl;
-
-	system("pause");
+	cout << endl;
+	//system("pause");
 	}
 
 	return 0;
